0270ce5c35bcbcb8c97464507fb50a7c7383aee2
Nevin Beno <206091677+nevinbeno@users.noreply.github.com>
2026-06-19 12:01:06 +0530

    add match edited

--- src/add_match.py
+++ src/add_match.py
@@ -1,29 +1,5 @@
 import mysql.connector
-from dotenv import find_dotenv, load_dotenv
-import os
-
-load_dotenv()
-
-conn = mysql.connector.connect(
-    host = os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWD"),
-    database=os.getenv("DB_NAME")
-)
-
-cursor = conn.cursor()
-
-print(f"==  Add Fixtures ==")
-
-n = int(input("No. of matches: "))
-for i in range (0, n):
-    print(f"Match {i +1}:")
-    team1 = input("Team 1: ")
-    team2 = input("Team 2: ")
-    cursor.callproc("add_match", [team1, team2])
-    conn.commit()
-import mysql.connector
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
@@ -58,5 +34,4 @@ finally:
     cursor.close()
     conn.close()
 
-print(f"{n} number of Matches added.")
-print(f"{n} number of Matches added.")
+print(f"{n} Matches added.")
